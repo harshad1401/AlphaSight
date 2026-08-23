7f4c0b33ee8be65ba24e5976524327f74f30f934
harshad1401 <hodageharshad01@gmail.com>
2026-08-23 18:50:51 +0530

    Calculate_Beta.py

--- pages/Calculate_Beta.py
+++ pages/Calculate_Beta.py
@@ -29,9 +29,18 @@ st.title("Calculate Beta and Return for Individual Stock")
 
 # Getting Input from User -
 col1, col2 = st.columns([1, 1])
+
+# Import Dataset -
+sp500 = pd.read_csv("sp500_companies.csv")
+
+# Display Companies -
+# st.dataframe(sp500)
+
+# Get ticker list -
+tickers_list = sp500['Symbol'].to_list()
+
 with col1:
-    stock = st.selectbox("Choose a Stocks", ['TSLA', 'AAPL', 'NFLX', 'MGM', 'AMZN', 'NVDA', 'GOOGL', 'META',
-                         'MSFT', 'LLY', 'AMD', 'V', 'XOM', 'JNJ', 'MU', 'CSCO', 'BAC', 'INTC', 'ORCL', 'DELL', 'GS', 'JCI'])
+    stock = st.selectbox("Choose a Stocks", tickers_list)
 with col2:
     year = st.number_input("Number of years: ", 1, 10)
 
