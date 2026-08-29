bc2e6d758c7471b732a9bfd55da5e44684a30282
harshad1401 <hodageharshad01@gmail.com>
2026-08-29 20:43:10 +0530

    Stock_Analysis.py

--- pages/Stock_Analysis.py
+++ pages/Stock_Analysis.py
@@ -22,19 +22,6 @@ st.set_page_config(
     layout="wide"
 )
 
-# CUSTOM CSS -
-st.markdown("""
-<style>
-
-.stApp {
-    background-color: #131722;
-    color: white;
-}
-
-</style>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-""", unsafe_allow_html=True)
-
 st.title("Stock Analysis")
 
 # Getting Input from User -
